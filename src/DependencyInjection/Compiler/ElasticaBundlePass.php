bf8049cb274511bafd7e7f9b0dc88b6f38d8c4a0
Dalibor Karlović <dalibor.karlovic@burza.hr>
2017-10-25 16:31:28 +0200

    WIP

--- src/DependencyInjection/Compiler/ElasticaBundlePass.php
+++ src/DependencyInjection/Compiler/ElasticaBundlePass.php
@@ -2,9 +2,17 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the RollerworksSearch package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Rollerworks\Bundle\SearchBundle\DependencyInjection\Compiler;
 
-use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
